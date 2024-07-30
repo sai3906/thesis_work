75ec88b1ebf2889f0e6471c52b2886b32bb3ef99
SAI KIRAN VALLAMKONDA <142433911+sai3906@users.noreply.github.com>
2024-07-30 16:50:54 +0530

    Add files via upload

---
+++ qpskmldecoder_july30_2.py
@@ -0,0 +1,66 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+import numpy as np
+
+
+def modulation(bits):
+    #mapping
+    mapping = {
+        (0,0): 1+1j,
+        (0,1):-1+1j,
+        (1,0):-1-1j,
+        (1,1): 1-1j
+    }
+
+    #modulation
+    modulated_bits=np.array([])
+    for i in range(0,len(bits),2):
+        bit_pair=(bits[i],bits[i+1])
+        modulated_bits=np.append(modulated_bits,mapping[bit_pair])
+    #print("mod",modulated_bits)
+    return modulated_bits
+
+def awgn(modulated_bits):
+    #adding noise
+    received_signal=np.array([])
+    for i in range(0,len(modulated_bits)):
+        a=modulated_bits[i].real + np.random.normal(0,1,1)
+        b=modulated_bits[i].imag + np.random.normal(0,1,1)
+        received_signal=np.append(received_signal, a+(1j * b))
+    #print("channel op ",received_signal,"\n")
+    return received_signal
+
+def demodulation(received_signal):
+    symbols = {
+        1 + 1j: (0, 0),
+        -1 + 1j: (0, 1),
+        1 - 1j: (1, 0),
+        -1 - 1j: (1, 1)
+    }
+
+    #demodulation
+    demodulated_bits=np.array([])
+    for i in received_signal:
+        closest_symbol = min(symbols.keys(), key=lambda s: np.abs(i - s))
+        demodulated_bits=np.append(demodulated_bits,(symbols[closest_symbol]))
+    #    print(np.array(demodulated_bits))
+
+    received_signal1=np.array([int(i)  for i in demodulated_bits])
+    #converting into int just for appearance
+    return received_signal1
+
+
+
+#enter the number of bits to transmit in even number
+no_of_bits=10
+bits=np.random.choice([0,1],no_of_bits)
+print("msg  bits",bits)
+
+modulated_bits=modulation(bits)
+
+channel_op=awgn(modulated_bits)
+
+demodulated_signal=demodulation(channel_op)
+
+print("rcvd bits",demodulated_signal)
