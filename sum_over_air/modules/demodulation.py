6c45208eefa750a851f63cd9d3f79dd7422eebe3
SAI KIRAN VALLAMKONDA <142433911+sai3906@users.noreply.github.com>
2024-10-15 12:44:04 +0530

    Update demodulation.py

--- sum_over_air/modules/demodulation.py
+++ sum_over_air/modules/demodulation.py
@@ -1,7 +1,7 @@
 import numpy as np
 def demod(received_signal,channel_coeff):
     # print("received:\n",received_signal,"\n")
-    a_opt=(np.sum(channel_coeff))/(np.sum(channel_coeff**2)+1)
+    a_opt=(np.sum(channel_coeff))/(np.sum(channel_coeff**2)+3)
     return received_signal*a_opt
 
 # ch=np.array([[0.2,-0.3],[0.1,0.5]])
